a65815fdd0395ae15d47e68658b3aa4c9ff33479
emily@baymax-desktop <emilylam@bu.edu>
2018-03-27 22:17:28 -0400

    some basic test code

---
+++ MATT-demo.js
@@ -0,0 +1,67 @@
+/*
+Matt-demo, routine
+Emily Lam, March 2018
+*/
+
+// Serial port
+var SerialPort = require('serialport');
+var port_xy = new SerialPort('/dev/cu.usbmodem14121', {
+  baudRate: 9600
+});
+
+// Use a Readline parser
+var parsers = SerialPort.parsers;
+var parser = new parsers.Readline({
+  delimiter: '\r\n'
+});
+
+// Port open errors will be emitted as an error event
+port_xy.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+
+// Swith port open to flowing mode and pipe to parser.
+port_xy.on('open', () => console.log('Port open'));
+// pipe to parser
+port_xy.pipe(parser);
+parser.on('data', console.log);
+
+setTimeout(start_loop, 2000);
+function start_loop() {setInterval(MATT_routine, 1000);}
+
+x = 0;
+y = 0;
+xy = 1; // xy = 1 move x, xy = 2 move y
+max_x = 10;
+max_y = 10;
+var msg;
+function MATT_routine() {
+  if (xy == 1) {
+    x = x + 1;
+    msg = "X-" + x + "\r";
+    if (x > max_x-1) {xy=2;}
+  }
+  else if (xy == 2) {
+    y = y + 1;
+    msg = "Y-" + y + "\r";
+    if (y > max_y-1) {xy = 3;}
+  }
+  else if (xy == 3) {
+    x = x - 1;
+    msg = "X-" + x + "\r";
+    if (x < 2) {xy = 4;}
+  }
+  else if (xy == 4) {
+    y = y - 1;
+    msg = "Y-" + y + "\r";
+    if (y < 2) {xy = 1;}
+  }
+
+
+  port_xy.write(msg, function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('Wrote: ',msg);
+  });
+}
